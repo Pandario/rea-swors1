8e371b52198b75ae67512314c7f94eb9f46218e2
Mary Vasileva <mary.vasileva@auriga.com>
2022-12-13 18:30:20 +0100

    7th commit popup form usestate made" q

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../assets/popup.css'
 
 const Popup  = ({open, onClose}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [text, setText] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {name, email, text};
+    console.log(blog);
+  };
+
   if (!open) return null;
+
   return (
+    
     <div className="popUp">
       <div className="close">
         <p className="closeBtn" onClick={onClose}>X</p>
       </div>
-      <div className="submitBtn">
-        <button className='submit'>
-          <span>JOIN</span>
-        </button>
-      </div>
+      <form className="formFields" onSubmit={handleSubmit}>
+        <div className="subscribePop">
+          Join Us!
+        </div>
+        <div className="namePop">
+        <label htmlFor="">Your name:  </label>
+          <input 
+            type="text" 
+            placeholder='Enter your name' 
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="emailPop">
+          <label htmlFor="" >Your email:  </label>
+          <input 
+            placeholder='Enter your email' 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="textPop">
+          <label htmlFor="" className='textPop1'>Enter text:  </label>
+          <textarea 
+            placeholder='Enter some text' 
+            className='textPop2'
+            cols="18" rows="8"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+          {console.log(name, email, text)}
+        </div>
+
+        <div className="submitBtn">
+          <button className='submit'>
+            <span>JOIN</span>
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
